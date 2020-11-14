db2915e69a17a43b6383dda269fa9c4c57f2d019
chip <onlygamernerd@gmail.com>
2020-11-14 17:38:29 +0100

    should have fxmanifest as well actualy

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fxversion "adamant"
+game "gta5"
+
+client_script {
+  "client/*.lua",
+}
+
+server_script {
+  "server/*.lua"
+}
+
+ui_page "html/index.html"
+
+files {
+  "html/index.html",
+  "html/main.js"
+}
