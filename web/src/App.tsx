13d611a9747dba3336715c9f15f57914941cda04
Christopher <onlygamernerd@gmail.com>
2020-12-13 14:11:30 +0100

    fix: should fix command not working to show nui

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,9 +7,9 @@ import { useVisibility } from './core/hooks/useVisibility';
 import { useCoreService } from './core/hooks/useCoreService';
 
 function App() {
-  const { visibility } = useVisibility()
   useNuiService();
   useCoreService();
+  const visibility = useVisibility()
   return (
     <div style={ visibility ? { visibility: 'visible' } : { visibility: 'hidden' }}>
      {/** Any types of components goes here. Maybe some routing? */}
--- web/src/core/hooks/useVisibility.ts
+++ web/src/core/hooks/useVisibility.ts
@@ -3,5 +3,5 @@ import { coreState } from './state';
 
 export const useVisibility = () => {
   const visibility = useRecoilValue(coreState.visibility);
-  return { visibility }
+  return visibility
 }
